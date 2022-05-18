0e6227e414249217f5ad827c1975247abfb2d242
hikaruikebe <hikaru.ikebe@columbia.edu>
2022-05-19 07:35:55 +0900

    updated plot

--- plot.py
+++ plot.py
@@ -1,13 +1,13 @@
 import matplotlib.pyplot as plt
 
-# from IPython import display
+from IPython import display
 
 plt.ion()
 
 
 def plot(scores, mean_scores, moving_mean_scores):
-    # display.clear_output(wait=True)
-    # display.display(plt.gcf())
+    display.clear_output(wait=True)
+    display.display(plt.gcf())
     plt.clf()
     plt.title("Training...")
     plt.xlabel("Number of Games")
